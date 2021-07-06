7488ffdba9c3ce75113b15fb4a7d045b8c444aa7
todo <todo@todo>
2021-07-06 14:00:22 +0800

    consumer init

---
+++ src/main/java/com/zhyyu/learn/rocketmq/consumer/LearnRocketMqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.zhyyu.learn.rocketmq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnRocketMqConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnRocketMqConsumerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/zhyyu/learn/rocketmq/consumer/config/ReceiveMsgConsumer.java
@@ -0,0 +1,41 @@
+package com.zhyyu.learn.rocketmq.consumer.config;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Component
+public class ReceiveMsgConsumer implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        // 实例化消费者
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name");
+
+        // 设置NameServer的地址
+        consumer.setNamesrvAddr("192.168.100.3:9876");
+
+        // 订阅一个或者多个Topic，以及Tag来过滤需要消费的消息
+        consumer.subscribe("TopicTest", "*");
+        // 注册回调实现类来处理从broker拉取回来的消息
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+//                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), new String(msgs.get(0).getBody(), StandardCharsets.UTF_8));
+                // 标记该消息已经被成功消费
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        // 启动消费者实例
+        consumer.start();
+        System.out.printf("Consumer Started.%n");
+    }
+}
